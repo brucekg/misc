38c9fcbd535318163f3baeac321add23196d1cf8
brucekg <brucekg@tradlos.net>
2021-12-16 13:36:59 -0500

    reworked AZ code and mods 'gpaz'

--- late.py
+++ late.py
@@ -1,6 +1,18 @@
 from math import sqrt
 from aobj import AObj
 
+# Landing Burn Factor
+LK = 4
+# Aerobrake/Hazard Factor
+AZK = 2
+
+def print_list(li):
+    for s in li:
+        print(f'{s.name:12s} {s.mods:5s} {s.r:7.3f}   n = {s.n:2d} ><'
+              f'  hy= {s.hy:1.0f}  sz= {s.sz:1.0f}  b= {s.b:2.0f}   l= {s.l:.1f}   hz= {s.hz:1.0f}')
+    print('\n=============================\n')
+    return
+
 with open('late.data', 'r') as f:
     site_list = list()
     for line in f:
@@ -27,9 +39,12 @@ with open('late.data', 'r') as f:
             values.append(0.0)
 
         hy, sz, b, l, hz = values
-        site.update_(hy=hy, sz=sz, b=b, l=l, hz=hz, gp=0)
         gp = 0
+        az = 0
+        mods = ''
+        site.update_(hy=hy, sz=sz, b=b, l=l, hz=hz, gp=gp, az=az, mods=mods)
 
+        az_limit = l * LK / AZK
         while tokens:
             token = tokens.pop(0)
             for c in token:
@@ -37,27 +52,37 @@ with open('late.data', 'r') as f:
                     assert gp == 0, f'GP error in line: {line}'
                     if c == 'g':
                         gp = 2 # group
-                    else:
+                        mods += 'G'
+                    if c == 'p':
                         gp = 1 # prospect
+                        mods += 'P'
                     site.gp = gp
-                elif c == 'a': # aerobrake
-                    l -= .5
-                elif c =='z': # hazard on aero
-                    l += .5
+                    site.mods = mods
 
-        # r = sqrt((1 + sz + g) / ((6 - hy) * (b + 4 * l + 4 * hz)))
-        r = sqrt(sz + gp) / sqrt((6 - hy) * (b + 1 + 4 * l + 4 * hz))
+                if c == 'a': # aerobrake cancelled landing burn
+                    l -= 1
+                    az = 1
+                    mods += 'A'
+                if c =='z': # extra hazard on aerobrake path
+                    az += 1
+                    mods += 'Z'
+                if c in 'az':
+                    site.az = az
+                    site.mods = mods
+
+        az = min(az, az_limit)
+        r = sqrt(sz + gp) / sqrt((6 - hy) * (b + 1 + LK * l + AZK * az))
         n = int(10 * r)
-        # print(f'{name:10s} {" PG"[gp]} {r:7.3f}   n = {n:2d}')
 
         site.r = r
         site.n = n
 
         site_list.append(site)
 
-print('=============================\n')
 
 sorted_list = sorted(site_list, key=lambda e: e.name)
-for s in sorted_list:
-    print(f'{s.name:12s} {" PG"[s.gp]} {s.r:7.3f}   n = {s.n:2d} ><'
-          f'  hy= {s.hy:1.0f}  sz= {s.sz:1.0f}  b= {s.b:2.0f}   l= {s.l:.1f}   hz= {s.hz:1.0f}')
+print_list(sorted_list)
+
+sorted_list = sorted(site_list, key=lambda e: e.r)
+print_list(sorted_list)
+
