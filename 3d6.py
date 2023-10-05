6222ed11f0bd1d366e48e97b50b5f8d27901c1b3
brucekg <brucekg>
2023-10-04 20:45:06 -0400

    sum shadow

--- 3d6.py
+++ 3d6.py
@@ -11,8 +11,8 @@ for a in range(1, 7):
             result[r] += 1
             total += 1
 
-sum = 0
+s = 0
 for i in range(3, 19):
     r = result[i]
-    sum += r
-    print(f'{i}\t{r}\t{r/total:.3f}\t{sum}\t{sum/total:.3f}')
+    s += r
+    print(f'{i}\t{r}\t{r/total:.3f}\t{s}\t{s / total:.3f}')
