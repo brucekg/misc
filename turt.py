d69574db6dbcff134baa70fc6e24663ecd07b87f
brucekg <brucekg>
2025-06-27 14:07:45 -0400

    pentagram

--- turt.py
+++ turt.py
@@ -1,4 +1,12 @@
 from turtle import *
 
-forward(100)
+forward(300)
+right(180)
+for i in range(5):
+    forward(500)
+    left(144)
+left(360/5)
+for i in range(5):
+    forward(309)
+    right(360 / 5)
 pass
