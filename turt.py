a6d6f13f088aec3b83a182d4c4eac67d6f48fcf2
brucekg <brucekg>
2025-06-27 14:15:16 -0400

    def pent

--- turt.py
+++ turt.py
@@ -1,12 +1,14 @@
+from symbol import return_stmt
 from turtle import *
 
-forward(300)
-right(180)
-for i in range(5):
-    forward(500)
-    left(144)
-left(360/5)
-for i in range(5):
-    forward(309)
-    right(360 / 5)
-pass
+def pent():
+    forward(300)
+    right(180)
+    for i in range(5):
+        forward(500)
+        left(144)
+    left(360/5)
+    for i in range(5):
+        forward(309)
+        right(360 / 5)
+    return
